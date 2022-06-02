610d33841a4d70701ae0d43d7cbc720e8ec1a0d1
Remi Cresson <remi.cresson@irstea.fr>
2022-06-02 16:35:40 +0200

    FIX: missing os import

--- decloud/preprocessing/sentinel2_prepare.py
+++ decloud/preprocessing/sentinel2_prepare.py
@@ -22,6 +22,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 """Pre-process one Sentinel-2 image"""
+import os
 import logging
 import argparse
 import otbApplication
